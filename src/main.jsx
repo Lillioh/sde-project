94a6681000cd5f6204897c8d8901dba93cb0b566
Lillioh <linesguide@gmail.com>
2025-04-22 21:31:21 +0800

    Updated Main.jsx
    added /login page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import CreateAccount from "./CreateAccount";
 import CreateAccount2 from "./CreateAccount2";
 import BuyerDashboard from "./buyerDashboard";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<App/>} />
         <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/create-account-2" element={<CreateAccount2 />} />
