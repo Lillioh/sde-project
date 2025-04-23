ce22e42f862b49fd779ed06af650fb6d2aa1bca1
Lillioh <linesguide@gmail.com>
2025-04-23 12:59:11 +0800

    Updated Create Account

--- src/CreateAccount.jsx
+++ src/CreateAccount.jsx
@@ -39,44 +39,94 @@ function CreateAccount() {
   return (
     <div
       style={{ backgroundImage: "url('/src/assets/bg-ustp.png')" }}
-      className="flex w-screen h-screen bg-cover bg-center bg-no-repeat items-center justify-end"
+      className="
+        flex
+        w-screen h-screen
+        bg-cover bg-center bg-no-repeat
+        items-center justify-end
+      "
     >
-      <div className="text-left text-[#DDA853] font-extrabold text-5xl leading-tight text-shadow-lg absolute bottom-20 left-10">
+      <div
+        className="
+          text-left text-[#DDA853] font-extrabold text-5xl leading-tight text-shadow-lg
+          absolute bottom-20 left-10
+        "
+      >
         <div>YOUR</div>
         <div>JOURNEY</div>
         <div>BEGINS HERE</div>
       </div>
 
-      <div className="flex flex-col w-[550px] h-[700px] p-8 mr-50 bg-[#F9F9F9] rounded-3xl shadow-2xl">
-        <h1 className="mb-2 text-4xl font-bold text-blue-900 text-center">
+      <div
+        className="
+          flex flex-col
+          w-[550px] h-[700px]
+          p-8 mr-50
+          bg-[#F9F9F9]
+          rounded-3xl
+          shadow-2xl
+        "
+      >
+        <h1
+          className="
+            mb-2
+            text-4xl font-bold text-blue-900 text-center
+          "
+        >
           Sign Up
         </h1>
-        <p className="text-sm text-center text-gray-700">
+        <p
+          className="
+            text-sm text-center text-gray-700
+          "
+        >
           Step {step}: {step === 1 ? "Account Credentials" : "Personal Details"}
         </p>
 
         {/* Pagination Dots */}
-        <div className="mt-2 flex mb-4 justify-center">
+        <div
+          className="
+            flex
+            mt-2 mb-4
+            justify-center
+          "
+        >
           <div
-            className={`w-2 h-2 mx-1 rounded-full ${
-              step === 1 ? "bg-blue-900" : "bg-gray-300"
-            }`}
+            className={`
+              w-2 h-2
+              mx-1
+              rounded-full
+              ${step === 1 ? "bg-blue-900" : "bg-gray-300"}
+            `}
           ></div>
           <div
-            className={`w-2 h-2 mx-1 rounded-full ${
-              step === 2 ? "bg-blue-900" : "bg-gray-300"
-            }`}
+            className={`
+              w-2 h-2
+              mx-1
+              rounded-full
+              ${step === 2 ? "bg-blue-900" : "bg-gray-300"}
+            `}
           ></div>
         </div>
 
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col gap-2 border-1 border-amber-300 space-y-4"
+          className="
+            flex flex-col
+            space-y-4
+            border-1 border-amber-300
+            gap-2
+          "
         >
           {step === 1 && (
             <>
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Username
                 </label>
                 <input
@@ -84,12 +134,23 @@ function CreateAccount() {
                   placeholder="Enter your username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Password
                 </label>
                 <input
@@ -97,12 +158,23 @@ function CreateAccount() {
                   placeholder="Enter your password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Confirm Password
                 </label>
                 <input
@@ -110,12 +182,23 @@ function CreateAccount() {
                   placeholder="Confirm your password"
                   value={confirmpassword}
                   onChange={(e) => setConfirm(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Email Address
                 </label>
                 <input
@@ -123,7 +206,13 @@ function CreateAccount() {
                   placeholder="Enter your email address"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
             </>
@@ -132,7 +221,12 @@ function CreateAccount() {
           {step === 2 && (
             <>
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   First Name
                 </label>
                 <input
@@ -140,12 +234,23 @@ function CreateAccount() {
                   placeholder="Enter your first name"
                   value={firstName}
                   onChange={(e) => setFirstName(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Last Name
                 </label>
                 <input
@@ -153,24 +258,46 @@ function CreateAccount() {
                   placeholder="Enter your last name"
                   value={lastName}
                   onChange={(e) => setLastName(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Date of Birth
                 </label>
                 <input
                   type="date"
                   value={dob}
                   onChange={(e) => setDob(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
 
               <div>
-                <label className="block text-sm font-medium text-gray-700 text-left">
+                <label
+                  className="
+                    block
+                    text-sm font-medium text-gray-700 text-left
+                  "
+                >
                   Address
                 </label>
                 <input
@@ -178,7 +305,13 @@ function CreateAccount() {
                   placeholder="Enter your address"
                   value={address}
                   onChange={(e) => setAddress(e.target.value)}
-                  className="w-full px-4 py-2 text-[14px] border rounded-md shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]"
+                  className="
+                    w-full
+                    px-4 py-2
+                    text-[14px]
+                    border rounded-md
+                    shadow-[inset_0px_2px_3px_rgba(0,0,0,0.2)]
+                  "
                 />
               </div>
             </>
@@ -186,7 +319,15 @@ function CreateAccount() {
 
           <button
             type="submit"
-            className="w-full py-2 mt-2 text-white font-semibold bg-blue-900 rounded-full shadow-md hover:bg-blue-800"
+            className="
+              w-full
+              py-2 mt-2
+              text-white font-semibold
+              bg-blue-900
+              rounded-full
+              shadow-md
+              hover:bg-blue-800
+            "
           >
             {step === 1 ? "Next" : "Submit"}
           </button>
@@ -195,7 +336,10 @@ function CreateAccount() {
             <button
               type="button"
               onClick={() => setStep(1)}
-              className="text-sm text-center text-blue-700 hover:underline"
+              className="
+                text-sm text-center text-blue-700
+                hover:underline
+              "
             >
               Back
             </button>
